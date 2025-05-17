2292b031c6c16fb4933b83b14000426f71cab457
Ayush Verma <ayushv2909@gmail.com>
2025-05-17 18:37:54 +0530

    fixing issues

--- server/index.js
+++ server/index.js
@@ -11,13 +11,7 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY
 const app = express();
 const port = 4000;
 
-
-app.use(
-  cors({
-    origin: "https://ayushverma2909.github.io",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
