787f3378b8b7c0241c619af5654d960afd994249
Ayush Verma <ayushv2909@gmail.com>
2025-05-18 13:10:22 +0530

    trailing trash

--- server/index.js
+++ server/index.js
@@ -11,7 +11,10 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY
 const app = express();
 const port = 4000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://ayushverma.github.io'],
+  credentials: true
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
